49c95828f2ad415bfb266d5525ee3888bb499178
nate-lev <115161847+nate-lev@users.noreply.github.com>
2022-12-04 20:37:33 +1100

    Update AzurePS_DataToCSV.ps1
    Updated script

--- AzurePS_DataToCSV.ps1
+++ AzurePS_DataToCSV.ps1
@@ -1,28 +1,38 @@
-﻿# Login with Azure account
+# Login with Azure account
 Connect-AzAccount
 
-# Get all subscriptions in tenant
-$azSubs = Get-AzSubscription
+# Get all resource groups
+$rgs = Get-AzResourceGroup
 
-# Create loop for each subscription in tenant
-foreach ($azsub in $azSubs) {
-    Select-AzSubscription -SubscriptionId $azsub.Id
-    $vms = Get-AzVM
-    # Separate variable for VM power status
-    $vmPower = Get-AzVM -Status
-    # Separate variables for retrieving VM network adapter and IP address
-    $vmNIC = $vms.NetworkProfile.NetworkInterfaces.id
-    $vmIP = (Get-AzNetworkInterface -Name $vmNIC).IpConfigurations.PrivateIpAddress
-    # Create loop for each vm and write out each required detail
-    foreach ($vm in $vms) {
-     $output=[pscustomobject]@{
-        'VMId' = $vm.VmId
-        'VMName' = $vm.Name
-        'VMSize' = $vm.HardwareProfile.VmSize
-        'VMOSType' = $vm.StorageProfile.OsDisk.OsType
-        'VMPowerState' = $vmPower.PowerState
-        'VMIPAddress' = $vmIP }
-    }
-    # Output info to csv
-    $output | Export-Csv "c:\coding\Task 2\ExportedData.csv" -Append -Force
+# Get all VMs
+$vms = Get-AzVM
+
+foreach ($vm in $vms) {
+   # Set separate variables for VM IP address
+   $networkProfile = $vm.NetworkProfile.NetworkInterfaces.id.Split("/") | Select -Last 1
+   $IPAddr = (Get-AzNetworkInterface -Name $networkProfile).IpConfigurations.PrivateIpAddress
+
+   # Set separate loop to get VM Power State
+   foreach ($rg in $rgs) {
+    $vmstatus = Get-AzVM -ResourceGroupName $rg.ResourceGroupName -status
+    $vmpower = $vmstatus.PowerState
+    
+    # Get tags of VMs based off resource group
+    $tags = Get-AzResource -ResourceGroupName $rg.ResourceGroupName -Name $vm.Name
+    $vmTags = $tags.Tags.Keys + $tags.Tags.Values
+   }
+
+   $output=[pscustomobject]@{
+       'VM Id' = $vm.VmId
+       'VM IP' = $IPAddr
+       'VM Name' = $vm.Name
+       'VM Size' = $vm.HardwareProfile.VmSize
+       'VM OS' = $vm.StorageProfile.OsDisk.OsType
+       'VM Power State' = $vmpower
+       'VM Tags' = ($vmTags -join ';')
+   }
+   # Output info to console
+   $output
+   # Output info to csv
+   $output | Export-Csv "C:\Users\Nate\Documents\PowerShell Projects\Azure PowerShell\ExportedData.csv" -Append
 }
